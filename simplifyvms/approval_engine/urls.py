8c64ab72961db72d3c70d85dc7f6307cbaaaf0e3
Shivam Mahajan <shivamm@simplifyvms.com>
2020-10-23 17:58:38 +0530

    second

--- simplifyvms/approval_engine/urls.py
+++ simplifyvms/approval_engine/urls.py
@@ -3,7 +3,16 @@ from approval_engine import views
 app_name = 'approval_engine'
 
 urlpatterns = [
-    path('<uuid:id>', views.ApprovalPendingView.as_view(), name='approval'),
-    # path('approval_entity', views.ApprovalEntityView.as_view(), name='status'),     
-    path('entity_id/<uuid:entity_id>', views.EntityPendingView.as_view(), name='entity_pending')
+    path(
+        '<uuid:id>',
+        views.ApprovalPendingView.as_view(),
+        name='approval'),
+    path(
+        'status/<uuid:entity_id>',
+        views.EntityStatusView.as_view(),
+        name='status'),
+    path(
+        'entity_id/<uuid:entity_id>',
+        views.EntityPendingView.as_view(),
+        name='entity_pending')
 ]
--- simplifyvms/approval_engine/views.py
+++ simplifyvms/approval_engine/views.py
@@ -19,13 +19,25 @@ from .utils import get_final_status, callback_function
 log = logging.getLogger(__name__)
 
 class ApprovalPendingView(APIView):
+    '''
+    This view help us to get all the pending approvals
+    and update the pending approval
+    '''
     def get_object(self, id):
+        '''
+        Return list of approvals of given id
+        Parameters: id
+        '''
         try:
             return ApprovalPending.objects.filter(approver_id=id)
         except:
             raise Http404
 
     def get(self, request, *args, **kwargs):
+        '''
+        Return the list of approvals based on given status
+        Parameters: status, id
+        '''
         id = self.kwargs['id']
         _status = request.GET.get('status')
         if _status:
@@ -39,6 +51,10 @@ class ApprovalPendingView(APIView):
         })
 
     def put(self, request, *args, **kwargs):
+        '''
+        Update the status of approval
+        Parameters: id
+        '''
         id = self.kwargs['id']
         try:
             obj = ApprovalPending.objects.get(id=id)
@@ -59,11 +75,68 @@ class ApprovalPendingView(APIView):
 
 
 class EntityPendingView(APIView):
+    '''
+    This view help us to get the entity object having 'pending' status
+    '''
     def get(self, request, *args, **kwargs):
+        '''
+        Return the entity object having status 'pending'
+        Parameters: entity_id
+        '''
         entity_id = self.kwargs['entity_id']
-        objs = ApprovalPending.objects.filter(approval_entity=entity_id, status='pending')
-        for obj in objs:
-            user_obj = get_user(settings.PROFILE_ENDPOINT, obj.approver_id)
-            obj.approver_id =  user_obj
-        serializer = EntityPendingSerializer(objs, many=True)
-        return Response(serializer.data)
+        try:
+            objs = ApprovalPending.objects.filter(
+                approval_entity=entity_id,
+                status='pending')
+            for obj in objs:
+                user_obj = get_user(settings.PROFILE_ENDPOINT, obj.approver_id)
+                obj.approver_id = user_obj
+            serializer = EntityPendingSerializer(objs, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({"error": "{}".format(error)},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class EntityStatusView(APIView):
+    '''
+    This view help us to get the overall status of any entity
+    '''
+    def get(self, request, *args, **kwargs):
+        '''
+        Get overall status of the entity.
+        if any one approval is pending then return pending
+        if any one approval is rejected then return rejected
+        Parameters: entity_id
+        '''
+        entity_id = self.kwargs['entity_id']
+        try:
+            objs = ApprovalPending.objects.filter(approval_entity=entity_id)
+            status_list = []
+            # Get the status of all approvals
+            for obj in objs:
+                status_list.append(obj.status)
+
+            log.info(
+                "Status of approvals belongs to entity is {}"
+                .format(status_list))
+
+            # Get the accumulative status
+            if 'rejected' in status_list:
+                final_status = 'rejected'
+            elif 'pending' in status_list:
+                final_status = 'pending'
+            elif 'approved' in status_list:
+                final_status = 'approved'
+            else:
+                final_status = 'No data found'
+            entity_data = ApprovalEntity.objects.get(id=entity_id)
+            serializer = ApprovalEntitySerializer(entity_data)
+            data = {'status': final_status}
+            data.update(serializer.data)
+
+            return Response(data, status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({"error": "{}".format(error)},
+                            status=status.HTTP_400_BAD_REQUEST)
